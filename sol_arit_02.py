e8d2f81db1bc08dc5f977be11579cc18be5f1acb
jonatas.88 <jonatas.88@gmail.com>
2021-08-08 23:04:05 -0300

    change variable

---
+++ sol_arit_02.py
@@ -0,0 +1,10 @@
+
+#x = int(input())
+x=49
+sq_init = x
+limite_valor = 10
+for i in range (1,x+1):
+    if i > limite_valor:
+        continue
+    print("N[{}] = {}".format(i-1, sq_init))
+    sq_init *= 2
